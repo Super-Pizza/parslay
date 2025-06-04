396b17a10874f058f51bde33ab34c56bb387b48c
Fabien Cournoyer <fabien@cournoyer.club>
2025-06-04 07:56:48 -0400

    First 7guis example

---
+++ examples/7guis-1-counter.rs
@@ -0,0 +1,24 @@
+use parslay::reactive::RwSignal;
+use parslay::{Rgba, WidgetExt};
+
+fn main() -> parslay::Result<()> {
+    parslay::launch(|| {
+        let mut counter = RwSignal::new(0);
+
+        parslay::hstack(
+            4,
+            (
+                parslay::dyn_label(move || format!("{counter}")).padding(4),
+                parslay::button("Count")
+                    .padding(4)
+                    .border_radius(4)
+                    .background_color(Rgba::hex("#C0C0C0").unwrap())
+                    .on_click(move |_, _| counter += 1),
+            ),
+        )
+        .padding(8)
+        .border_radius(8)
+        .background_color(Rgba::WHITE)
+    })?;
+    Ok(())
+}
